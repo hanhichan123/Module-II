29ecaa5b9dc98ac2ec289e0ceed67f172d4896a6
hanhichan123 <bokaka.95@gmail.com>
2025-11-15 20:52:09 +0900

    Add files via upload

--- BAITAPSS2-6.sql
+++ BAITAPSS2-6.sql
@@ -1,58 +1,70 @@
-CREATE DATABASE TMDT;
-USE TMDT;
+CREATE DATABASE QLKH6;
+USE QLKH6;
 
 
-
-
--- TẠO BẢNG Customers
 CREATE TABLE Customers(
 CustomerID INT PRIMARY KEY AUTO_INCREMENT,
-CustomerNAME VARCHAR(100),
-Email VARCHAR(100),
+CustomerName VARCHAR (100) NOT NULL UNIQUE,
+Email VARCHAR(100) NOT NULL UNIQUE,
 Phone VARCHAR(15)
-);
+); 
 
--- TẠO BẢNG Orders
+
 CREATE TABLE Orders(
 OrderID INT PRIMARY KEY AUTO_INCREMENT,
-OrderDate DATE,
+OrderDATE DATE,
 CustomerID INT NOT NULL,
 FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
--- TẠO BẢNG OrderDetails
+
 CREATE TABLE OrderDetails(
-OrderDetailID INT PRIMARY KEY AUTO_INCREMENT,
+OrderdeailID INT PRIMARY KEY AUTO_INCREMENT,
 OrderID INT NOT NULL,
 FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-ProductID INT NOT NULL,
+ProductID INT,
 Quantity INT,
-UnitPrice DECIMAL(10,2),
-CHECK (UnitPrice >= 0)
+UnitPrice DECIMAL(10,2)
 );
 
-INSERT INTO Customers (CustomerNAME, Email, Phone)
-VALUES 
-('Nguyen Van A', 'a@example.com', '0901111111'),
-('Tran Thi B', 'b@example.com', '0902222222');
 
-INSERT INTO Orders (OrderDate, CustomerID)
-VALUES
-('2025-11-10', 1),
-('2025-11-11', 1),
-('2025-11-12', 2);
+-- Chèn 2 khách hàng vào bảng Customers
 
-INSERT INTO OrderDetails (OrderID, ProductID, Quantity, UnitPrice)
-VALUES
-(1, 1 , 2, 200),
-(1, 2 , 1, 500),
-(2, 1, 1, 800),
-(2, 2, 1, 400),
-(3, 1, 2, 150);
+INSERT INTO Customers(CustomerName,Email,Phone)
+ VALUES ('Nguyen Van A','ABC@gmail.com',090334443),
+ ('Tran Van B','CD@gmail.com',090344334);
+ 
+ -- Chèn ít nhất 3 đơn hàng vào bảng Orders
+ 
+ INSERT INTO Orders(OrderDATE,CustomerID)
+ VALUES ('2025-12-11', 1),
+ ('2025-10-20', 1),
+ ('2025-12-5', 2);
+ 
+-- Chèn ít nhất 5 chi tiết đơn hàng vào bảng OrderDetails
+INSERT INTO OrderDetails(OrderID,ProductID,Quantity,UnitPrice)
+ VALUES (1, 1, 2,100),
+ (1, 2, 1,300),
+ (2, 3, 0,200),
+ (2, 4, 3,500),
+ (3, 1, 2,100);
+ 
+ -- cập nhật số điện thoại của 1 khách hàng cụ thể 
+ 
+ UPDATE Customers SET Phone = 0700777777 WHERE CustomerID = 1;
+ 
+ -- xóa tất cả đơn hàng của khách hàn không còn hoạt động 
+ 
+ DELETE FROM Customers WHERE CustomerID = 3;
+ 
+ -- truy vấn để lấy danh sách toàn bộ khách hàng cùng tổng đơn hàng mà họ đã đặt 
+SELECT 
+    c.CustomerID,
+    c.CustomerName,
+    COUNT(o.OrderID) AS TotalOrders
+FROM Customers c
+LEFT JOIN Orders o 
+    ON c.CustomerID = o.CustomerID
+GROUP BY 
+    c.CustomerID, c.CustomerName;
 
-SELECT * FROM Customers;
--- CẬP NHẬT SỐ ĐIỆN THOẠI CỦA 1 KHÁCH HÀNG TRONG CUSTOMER
-UPDATE Customers SET Phone = '090900009' WHERE CustomerID = 1;
--- XÓA 1 KHÁCH HÀNG
-DELETE FROM Customers WHERE CustomerID = 4;
-SELECT * FROM Orders;
